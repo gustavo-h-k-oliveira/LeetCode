0960276545326a7d20b77dc815ad8a5381c846cc
gustavo-h-k-oliveira <gustavo.oliveira204@fatec.sp.gov.br>
2025-03-26 12:30:40 -0300

    Adicionando método "One-pass Hash Table" e atualizando organização de pastas.

--- Two Sum/twoSum.java
+++ Two Sum/twoSum.java
@@ -42,16 +42,35 @@ class Solution {
         return new int[] {};
     }
 
-    public static void main(String[] args) {
-        int[] nums = {2, 7, 11, 15}; // [0, 1]
-        int target = 9;
+    /*
+     * One-pass Hash Table
+     * Time Complexity: O(n)
+     * Space Complexity: O(n)
+     */
 
-        // int[] nums = {3,2,4}; // [1, 2]
-        // int target = 6;
+    public int[] OnePassHashTable(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        return new int[] {};
+    }
+
+    public static void main(String[] args) {
+        // int[] nums = {2, 7, 11, 15}; // [0, 1]
+        // int target = 9;
+
+        int[] nums = {3,2,4}; // [1, 2]
+        int target = 6;
 
         Solution sol = new Solution();
         // int[] result = sol.twoSum(nums, target);
-        int[] result = sol.TwoPassHashTable(nums, target);
+        // int[] result = sol.TwoPassHashTable(nums, target);
+        int[] result = sol.OnePassHashTable(nums, target);
 
         System.out.println(Arrays.toString(result));
     }
